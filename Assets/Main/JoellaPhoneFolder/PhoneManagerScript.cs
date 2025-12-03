15f9cbaea2021ecd9755ca871891d88ae549b1dd
xSpr0ut <Joellaxlin@gmail.com>
2025-12-03 12:42:24 -0500

    WORKING PHONE! WITH DIALOGUE AND OLAF + PAPARAZZI CALLS INCLUDED! YAYAYAYAYAY!

--- Assets/Main/JoellaPhoneFolder/PhoneManagerScript.cs
+++ Assets/Main/JoellaPhoneFolder/PhoneManagerScript.cs
@@ -1,23 +1,51 @@
 using System;
 using UnityEditor.Search;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using Ink.Runtime;
 
 public class PhoneManagerScript : MonoBehaviour
 {
 
-    [SerializeField]private GameObject phonePanel;
-    [SerializeField]private GameObject dialogueSystem;
-    private bool olafKannaContact = false;
-    private bool paparazziContact = false;
-    private Boolean fanContact = false;
+    [SerializeField] private GameObject phonePanel;
+    [SerializeField] private GameObject dialogueSystem;
+    [SerializeField] private GameObject Characters;
+    [SerializeField] private DialogueManager dialogueManager;
+
+    // variables for which contacts are available
+    /*
+    public bool olafKannaContact = false;
+    public bool paparazziContact = false;
+    public bool fanContact = false;
+    */
+    private String currentPhoneCall = "";
+
+    // INK STUFF
+    public TextAsset inkFile;
+    Story story;
+    TextArchitect architect;
+    bool activeDialogue = false;
+    Character activeCharacter;
+    public Transform choicePanel;
+    public GameObject choiceButtonPrefab;
 
 
     // ensures when the scene starts, the phone is off
-    void Awake()
+    void Start()
     {
         phonePanel.SetActive(false);
+
+        // Connect Architect to your DialogueSystem text
+        architect = new TextArchitect(DialogueSystem.instance.dialogueContainer.dialogueText);
+
         dialogueSystem.SetActive(false);
+
+         // Create Ink story
+        story = new Story(inkFile.text);
+
+        //Automatically go to first line
     }
 
     public void OpenPhone()
@@ -40,18 +68,38 @@ public class PhoneManagerScript : MonoBehaviour
         OpenPhone();
     }
 
+      public void PlayKnot(string Name)
+    {
+        if (story == null)
+        {
+            Debug.Log("Not initiallized");
+            return;
+        }
+
+       story.ResetState();
+        story.ChoosePathString(Name);
+
+        activeDialogue = true;
+        dialogueSystem.SetActive(true);
+
+        DialogueSystem.instance.dialogueContainer.dialogueText.text = "";
+        AdvanceStory();
+    }
+
     public void OnOlafButtonDown()
     {
         
-        dialogueSystem.SetActive(true);
+    //   dialogueSystem.SetActive(true);
+        Characters.SetActive(false);
+        PlayKnot("OlafPhoneCall");
+     //   dialogueManager.ShowFirstLine();
         ClosePhone();
         
     }
 
     public void OnPaparazziButtonDown()
     {
-        
-        dialogueSystem.SetActive(true);
+        PlayKnot("DavidYellowPhoneCall");
         ClosePhone();
         
     }
@@ -62,6 +110,138 @@ public class PhoneManagerScript : MonoBehaviour
         dialogueSystem.SetActive(true);
         ClosePhone();
         
+    }
+
+
+    // DIALOGUE SCRIPT
+
+    private void Update()
+    {
+        if (!activeDialogue)
+            return;
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (architect.isBuilding)
+            {
+                if (!architect.hurryUp)
+                    architect.hurryUp = true;
+                else
+                    architect.ForceComplete();
+            }
+
+            else
+            {
+                AdvanceStory();
+            }
+        }
+
+    }
+        void AdvanceStory()
+    {
+        // If Ink has more text
+        if (story.canContinue)
+        {
+            string line = story.Continue();
+            
+            ApplyTags();
+
+            architect.Build(line);
+            return;
+        }
+
+        // If no more text but there are choices
+        if (story.currentChoices.Count > 0)
+        {
+            ShowChoices();
+            return;
+        }
+
+       // Debug.Log("END OF STORY");
+       EndDialogue();
+    }
+
+    void EndDialogue()
+    {
+        activeDialogue = false;
+        dialogueSystem.SetActive(false);
+
+        Debug.Log("END OF STORY");
+    }
+
+    void ApplyTags()
+    {
+        foreach (string tag in story.currentTags)
+        {
+            string[] split = tag.Split(' ');
+
+            if (split.Length < 2)
+                continue;
+
+            string prefix = split[0].ToLower();
+            string param = split[1];
+
+            switch (prefix)
+            {
+                case "speaker":
+                    SetSpeaker(param);
+                    break;
+
+                case "expression":
+                    SetExpression(param);
+                    break;
+            }
+        }
+    }
+
+    void SetSpeaker(string speakerName)
+    {
+        activeCharacter = CharacterManager.Instance.GetCharacter(speakerName);
+
+        if (activeCharacter != null)
+        {
+            DialogueSystem.instance.dialogueContainer.nameText.text = activeCharacter.characterName;
+        }
+        else
+        {
+            Debug.LogWarning("No character found: " + speakerName);
+        }
+    }
+
+    void SetExpression(string expression)
+    {
+        if (activeCharacter != null)
+        {
+            activeCharacter.SetExpression(expression);
+        }
+    }
+
+        // ---- Choices ---- //
+    void ShowChoices()
+    {
+        choicePanel.gameObject.SetActive(true);
+
+        // Clear old buttons
+        foreach (Transform child in choicePanel)
+            Destroy(child.gameObject);
+
+        // Create a button for each choice
+        for (int i = 0; i < story.currentChoices.Count; i++)
+        {
+            Choice c = story.currentChoices[i];
+            GameObject buttonObj = Instantiate(choiceButtonPrefab, choicePanel);
+            ChoiceButton btn = buttonObj.GetComponent<ChoiceButton>();
+            btn.Init(c.text, i, OnChoiceSelected);
+        }
+    }
+
+    void OnChoiceSelected(int choiceIndex)
+    {
+        choicePanel.gameObject.SetActive(false);
+
+        story.ChooseChoiceIndex(choiceIndex);
+
+        AdvanceStory();
     }
 
 
